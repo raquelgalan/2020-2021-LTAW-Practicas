95328b32a9116cf782a2da349aa55cbdfe84c18a
raquelgalan <raquel.galan.montes@gmail.com>
2021-04-06 14:32:41 +0200

    crear servidor web

---
+++ P1/tienda.js
@@ -0,0 +1,19 @@
+//-- Declarar constantes
+const http = require('http'); //-- Módulo http
+const fs = require('fs'); //-- Módulo fs
+const PUERTO = 9000; //-- Puerto a utilizar
+
+
+
+//-- Crear el servidor. Por cada petición se imprime mensaje.
+const server = http.createServer((req, res) => {
+    
+    //-- Indicamos que se ha recibido una petición
+    console.log("Petición recibida!");
+  });
+
+
+//-- Activar servidor, escuchando peticiones en el puerto.
+server.listen(PUERTO);
+
+console.log("Servidor activado. Escuchando en el puerto: " + PUERTO);
