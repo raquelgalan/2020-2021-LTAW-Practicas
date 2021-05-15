49745846068751bc901d9a44f050090d5da0fb63
raquelgalan <raquel.galan.montes@gmail.com>
2021-05-15 13:44:00 +0200

    Ejemplos: HTML

---
+++ P1/S3.Ejemplos/HTML_Ejemplo6.js
@@ -0,0 +1,37 @@
+// no es texto plano como anteriores ejemplos, es texto html
+const http = require('http');
+
+const PUERTO = 8080;
+
+//-- Texto HTML
+const pagina = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>¡Happy Server!</title>
+</head>
+<body style="background-color: lightblue">
+    <h1 style="color: green">HAPPY SERVER!!!</h1>
+</body>
+</html>
+`
+//viewport hace que se vea bien en los teléfonos móviles
+//no meter el style dentro de html, tiene que ir en fichero a parte.
+
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    res.statusCode = 200;
+    res.statusMessage = "OK";
+    res.setHeader('Content-Type','text/html');
+    res.write(pagina);
+    res.end();
+});
+
+server.listen(PUERTO);
+
+console.log("Ejemplo 6. Happy Server HTML!. Escuchando en puerto: " + PUERTO);
